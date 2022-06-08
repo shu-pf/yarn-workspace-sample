62a6c02e2331be55a3cc690a863394b46e67df1e
shu-pf <s_furuse@yumemi.co.jp>
2022-06-09 03:38:48 +0900

    SampleComponentの読み込み

--- packages/my-app/pages/index.js
+++ packages/my-app/pages/index.js
@@ -1,3 +1,10 @@
+import { SampleComponent } from "shared/components/SampleComponent";
+
 export default function Home() {
-  return <h1>my-app</h1>;
+  return (
+    <main>
+      <h1>my-app</h1>
+      <SampleComponent></SampleComponent>
+    </main>
+  );
 }
